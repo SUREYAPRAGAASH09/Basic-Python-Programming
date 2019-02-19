8e2b7f34456cdc0168531caf1d146ad6623ac810
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-19 23:20:11 +0530

    Code Revised Completely

--- Exercise/Even_odd_lenght_palindrome/reverse.py
+++ Exercise/Even_odd_lenght_palindrome/reverse.py
@@ -1,13 +1,18 @@
-#this function is to reverse a number
-
-def reversess(N):
-    rev_num = 0
-    while (N>0):
-        temp = N % 10
-        rev_num = (rev_num*10)+temp
-        N = N // 10
-    return rev_num
-
-#print(reversess(1234))
-
-
+Problem Statement :
+===================    
+    This function is to reverse a number
+input :
+=======
+    Integer - Number 
+Output :
+========
+    Reversed Number - Integer
+Code :
+=======    
+def reversess(Number):
+    reversedNumber = 0
+    while (Number>0):
+        temp = Number % 10
+        reversedNumber = (reversedNumber*10)+temp
+        Number = Number // 10
+    return reversedNumber
