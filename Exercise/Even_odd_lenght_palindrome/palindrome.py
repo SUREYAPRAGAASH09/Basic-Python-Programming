32b613e2fc678c3ee692f9ed76236c5fec048adf
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-19 23:17:14 +0530

    Code Revised Completely

--- Exercise/Even_odd_lenght_palindrome/palindrome.py
+++ Exercise/Even_odd_lenght_palindrome/palindrome.py
@@ -1,11 +1,20 @@
-#To check whether the given number is palindrome or not 
+Problem Statement :
+===================
+    To check whether the given number is palindrome or not 
+Input :
+=======
+    Number - Integer
+Output :
+========
+    Flag - True if palindrome exsist - Boolean
+    Flag - False if palindrome does Not exsist - Boolean
+Code :
+=======
 import reverse
-def palindrome(N):
-    Num  = N
-    N = reverse.reversess(N) 
+def palindrome(Number):
+    actualNum  = Number
+    reversedNumber = reverse.reversess(N) 
     flag = False
-    if Num == N:
+    if actualNum == reversedNumber:
         flag = True
     return flag
-
-#print(palindrome(343))
