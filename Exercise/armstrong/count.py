313e9cc4a41d5c40b8d64dc3acfed2ef85ab9fa0
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-19 23:28:31 +0530

    Code Revised Completely

--- Exercise/armstrong/count.py
+++ Exercise/armstrong/count.py
@@ -1,18 +1,13 @@
-#To count the no of digits given in a number
-# def countnumbers(n):
-#     count = 0
-#     while (n>0):
-#         temp = int(n) % 10
-#         count += 1
-#         n = int(n) / 10
-#         n = int(n)
-
-#     return count
-#another variant of solving the above problem
-def countnumbers(n):
-    n = list(str(n))
-    n = len(n)
-    return n
-    
-
-#print(countnumbers(1234512345))
+Problem Statement :
+===================    
+    To count the no of digits given in a number
+Input :
+========
+     Integer - number 
+Output :
+=======
+    Count Value - integer 
+Code :
+=======    
+def countnumbers(number):
+    return len(list(ste(number)))
