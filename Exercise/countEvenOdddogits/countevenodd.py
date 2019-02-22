53edeea087c19f6336ea1692af33e5e0b57ced38
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-22 21:01:27 +0530

    Code Revised Completely

--- Exercise/countEvenOdddogits/countevenodd.py
+++ Exercise/countEvenOdddogits/countevenodd.py
@@ -1,15 +1,24 @@
-#to find count the number of odd and even number in a given integer
-def countevenodd(n):
+Problem Statement :
+===================    
+    to find count the number of odd and even number in a given integer
+Input :
+=======
+    Integer Number 
+Output :
+========
+    Even Count and Odd Count - Integer
+Code :
+======    
+def countevenodd(number):
     even = 0
     odd = 0
-    while(n>0):
-        temp = int(n) % 10
+    while(number>0):
+        temp = number % 10
         if (temp%2==0):
             even += 1
         else:
             odd += 1
-        n = int(n) / 10
-        n = int(n)
-    return even,odd
+        number = number / 10
+            return even,odd
 
-print(countevenodd(12345))
+        
