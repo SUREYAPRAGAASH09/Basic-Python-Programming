1e894678b4f3b7fa3a7b1bf0f3fc2d29cc95503f
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-22 21:07:35 +0530

    Changes

--- Exercise/getFirstLastNumber/count.py
+++ Exercise/getFirstLastNumber/count.py
@@ -1,14 +1,14 @@
-#To count the no of digits given in a number
-# def countnumbers(n):
-#     count = 0
-#     while (n>0):
-#         temp = int(n) % 10
-#         count += 1
-#         n = int(n) / 10
-#         n = int(n)
+To count the no of digits given in a number
+ def countnumbers(n): 
+     count = 0
+     while (n>0):
+         temp = int(n) % 10
+         count += 1
+         n = int(n) / 10
+         n = int(n)
 
-#     return count
-#another variant of solving the above problem
+     return count
+another variant of solving the above problem
 def countnumbers(n):
     n = list(str(n))
     n = len(n)
