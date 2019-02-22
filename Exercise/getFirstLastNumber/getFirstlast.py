ec13151672a27b5f1260afd0075c03e810af3abb
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2019-02-22 21:15:26 +0530

    Code Revised Completely

--- Exercise/getFirstLastNumber/getFirstlast.py
+++ Exercise/getFirstLastNumber/getFirstlast.py
@@ -1,15 +1,23 @@
-#to get First and last number from the given number
+Problem Statement :
+===================    
+      to get First and last number from the given number
+Input :
+=======
+    Number - Integer 
+Output :
+=======
+    Get first integer and last integer - integer 
+Code :
+=====    
 import count
-def gettingFirstLastNumber(n): 
-    counti = count.countnumbers(n)
-    counti = counti 
+def gettingFirstLastNumber(num): 
     check = 10
     check_1 = check
     check_first = 1
-    for i in range(1,counti):
+    for i in range(1,count.countnumber(n)):
         check_first = check * check_first
     first =  n / check_first
     last = n % check_1
     return int(first),int(last)
 
-print(gettingFirstLastNumber(123456789))   
+ 
